de9171826797e4f62e1b5f6a1750147d32a5d83d
Nevilmehta <nevilmehta5@gmail.com>
2025-03-02 15:25:28 +0000

    Backend database commit

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from flask_migrate import Migrate 
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -16,6 +18,7 @@ def create_app():
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+    migrate.init_app(app, db)
 
     #blueprint for authentication
     from app.routes.auth import auth_bp
--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,10 @@
-class config:
-    SECRET_KEY = "secret"
-    SQLALCHEMY_DATABASE_URI = "database"
-    SQLALCHEMY_TRACK_MODIFICATIONS = FALSE
-    JWT_SECRET_KEY = "jwt_secret"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
